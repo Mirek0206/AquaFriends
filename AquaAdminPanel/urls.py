a23af32725f4f8ac3a1b19e94210c680ece07531
maresyp <mattq.2000@icloud.com>
2024-05-31 13:06:47 +0200

    improved admin panel view

--- AquaAdminPanel/urls.py
+++ AquaAdminPanel/urls.py
@@ -4,4 +4,6 @@ from .views import admin_login, admin_panel
 urlpatterns = [
     path('aap/login', admin_login, name='admin_login'),
     path('aap/panel', admin_panel, name='admin_panel'),
+    path('aap/panel/<str:module_name>', admin_panel, name='admin_panel_model'),
+    path('aap/panel/<str:module_name>/<str:pk>', admin_panel, name='admin_panel_details'),
 ]
--- AquaAdminPanel/views.py
+++ AquaAdminPanel/views.py
@@ -1,8 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import Model
+
+from .admin import AquaAdminPanel
 
 # Create your views here.
 
@@ -36,5 +40,21 @@ def admin_login(request):
 
 
 @staff_member_required(login_url="admin_login")
-def admin_panel(request):
-    return render(request, "AquaAdminPanel/panel.htm")
+def admin_panel(request, module_name: str = None, pk: str = None):
+    context: dict = {
+        "modules": [key for key in AquaAdminPanel().registry.keys()]
+    }
+
+    if module_name:
+        model: Model | None = AquaAdminPanel().registry.get(module_name)
+        if model is None:
+            messages.error(request, 'Taki model nie istnieje')
+            return redirect("admin_panel")
+
+        context["model"] = model.__name__
+        context["instances"] = model.objects.all()
+
+    if pk:
+        ...
+
+    return render(request, "AquaAdminPanel/panel.htm", context=context)
