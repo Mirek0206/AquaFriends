a6f3360a45492b5513a8147f2196b731c22db728
maresyp <mattq.2000@icloud.com>
2024-06-08 20:54:47 +0200

    add rest of the models to admin panel

--- AquaLife/admin.py
+++ AquaLife/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
+from AquaAdminPanel.admin import AquaAdminPanel
 from AquaLife.models import Fish, Species
 
 admin.site.register(Species)
 admin.site.register(Fish)
+
+AquaAdminPanel().register(Species)
+AquaAdminPanel().register(Fish)
--- AquaMaker/admin.py
+++ AquaMaker/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Aquarium, Pump, Light, Heater, Filter
+
+from AquaAdminPanel.admin import AquaAdminPanel
+
+from .models import Aquarium, Filter, Heater, Light, Pump
 
 admin.site.register(Aquarium)
 admin.site.register(Pump)
 admin.site.register(Light)
 admin.site.register(Heater)
 admin.site.register(Filter)
+
+AquaAdminPanel().register(Aquarium)
+AquaAdminPanel().register(Pump)
+AquaAdminPanel().register(Light)
+AquaAdminPanel().register(Heater)
+AquaAdminPanel().register(Filter)
