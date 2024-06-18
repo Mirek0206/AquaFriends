9835755f6b84f6fa581f6eb12c1e9905aa4fd222
Pavlucek <p.morozewicz99@gmail.com>
2024-06-18 15:03:07 +0200

    Added AquaViewMatchShare to admin panel.

--- AquaViewMatchShare/admin.py
+++ AquaViewMatchShare/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Message
+from AquaAccount.models import Message
+from AquaAdminPanel.admin import AquaAdminPanel
 
 # Register your models here.
 admin.site.register(Message)
+AquaAdminPanel().register(Message)
