67f4edecddc9bfc7a7a548bf2442a1c4770af316
gruszm <mariuszgruszczynski11@gmail.com>
2024-06-20 17:29:36 +0200

    Generator repaired

--- AquaMonitor/models.py
+++ AquaMonitor/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 from AquaMaker.models import Aquarium
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class WaterParameter(models.Model):
+    aquarium = models.ForeignKey(Aquarium, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    no2 = models.FloatField(validators=[MinValueValidator(0.0)])
+    no3 = models.FloatField(validators=[MinValueValidator(0.0)])
+    gh = models.FloatField(validators=[MinValueValidator(0.0)])
+    kh = models.FloatField(validators=[MinValueValidator(0.0)])
+    ph = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(14.0)])
+
+    def __str__(self):
+        return f'Parameters for {self.aquarium.name} on {self.date.strftime("%Y-%m-%d")}'
 
 class SituationType(models.Model):
     name = models.CharField(max_length=50)
